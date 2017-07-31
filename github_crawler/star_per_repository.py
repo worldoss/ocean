a7617d2c880c49091f8721d3b6b517c3aa1a52bb
rlrlaa123 <kimdonghyun3366@gmail.com>
2017-07-31 21:35:54 +0900

    update star_per_repository with language

--- github_crawler/star_per_repository.py
+++ github_crawler/star_per_repository.py
@@ -1,8 +1,23 @@
+#-*- coding: utf-8 -*-
 import httplib2
 import json
 import base64
 import csv
-import time
+from time import sleep
+
+class NoincompleteError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+class NoresultError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
 
 def request(url):
     http = httplib2.Http()
@@ -11,21 +26,59 @@ def request(url):
     auth = base64.encodestring(id + ':' + pw)
     return http.request(url,'GET',headers={ 'Authorization' : 'Basic ' + auth})
 
-def json_count(data):
-    return json.loads(data)['total_count']
+# 언어별 1000번째 저장소 star수
+lang_thousand={
+    'ActionScript':10,
+    'C 34455':428,
+    'CSharp':227,
+    'CPP':421,
+    'Clojure':64,
+    'CoffeeScript':63,
+    'CSS':262,
+    'Go':475,
+    'Haskell':35,
+    'HTML':322,
+    'Java':1270,
+    'Javascript':2908,
+    'Lua':36,
+    'Matlab':9,
+    'Objective-C':716,
+    'Perl':32,
+    'PHP':458,
+    'Python':1012,
+    'R':30,
+    'Ruby':600,
+    'Scala':75,
+    'Shell':215,
+    'Swift':359,
+     'TeX':15,
+     'Vim-script':59
+}
 
-count=1
-while count<=6587:
-    url = 'https://api.github.com/search/repositories?q=stars:'+str(count)+'&per_page=100'
-    response, content = request(url)
-    if json.loads(content)['incomplete_resuts'] == False:
-        json_parsed = json_count(content)
-        print count, json_parsed
-        if int(json_parsed) != 0:
-            with open('data/star_per_repository.csv','a') as csvfile:
-                writer= csv.writer(csvfile)
-                writer.writerow([str(count)]+[json_parsed])
-                count+=1
-    else:
-        pass
-    time.sleep(2)
+lang_items=lang_thousand.items()
+for lang in lang_items:
+    count=lang[1]-1
+    while count>5:
+        url = 'https://api.github.com/search/repositories?q=stars:'+str(count)+'+language:"'+lang[0]+'"&per_page=100'
+        print url
+        try:
+            response, content = request(url)
+            json_parsed =  json.loads(content)['total_count']
+            if json.loads(content)['incomplete_results'] == False:
+                if int(json_parsed) != 0:
+                    print count, json_parsed
+                    with open('data/star_per_repository_language.csv','a') as csvfile:
+                        writer= csv.writer(csvfile)
+                        writer.writerow([str(count)]+[json_parsed])
+                        count-=1
+                else:
+                    raise NoresultError('No results')
+            else:
+                raise NoincompleteError('Incomplete results, try again')
+        except NoincompleteError as e:
+            print e
+        except NoresultError as e:
+            count-=1
+            print e
+        except KeyError:
+            sleep(1)
