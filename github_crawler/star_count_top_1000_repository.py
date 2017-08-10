d93646c517d36cc1286e663be4e33f74f7f8bb86
rlrlaa123 <kimdonghyun3366@gmail.com>
2017-08-10 18:14:01 +0900

    update star_count_top_1000_repository

--- github_crawler/star_count_top_1000_repository.py
+++ github_crawler/star_count_top_1000_repository.py
@@ -21,7 +21,7 @@ class NoresultError(Exception):
     def __str__(self):
         return self.msg
 
-language=[
+lang_popular=[
     'ActionScript',
     'C',
     'CSharp',
@@ -48,6 +48,63 @@ language=[
     'TeX',
     'Vim-script'
 ]
+lang_others=[
+        '1C-Enterprise', 'ABAP', 'ABNF', 'Ada', 'Agda',
+        'AGS-Script', 'Alloy', 'Alpine-Abuild', 'AMPL',
+        'Ant-Build-System', 'ANTLR', 'ApacheConf', 'Apex',
+        'API-Blueprint', 'APL', 'Apollo-Guidance-Computer',
+        'AppleScript', 'Arc', 'Arduino', 'AsciiDoc', 'ASN.1',
+        'ASP', 'AspectJ', 'Assembly', 'ATS', 'Augeas', 'AutoHotkey',
+        'AutoIt', 'Awk', 'Batchfile', 'Befunge', 'Bison', 'BitBake',
+        'Blade', 'BlitzBasic', 'BlitzMax', 'Bluespec', 'Boo',
+        'Brainfuck', 'Brightscript', 'Bro', 'C-ObjDump', 'C2hs-Haskell',
+        "Cap'n-Proto", 'CartoCSS', 'Ceylon', 'Chapel', 'Charity', 'ChucK',
+        'Cirru', 'Clarion', 'Clean', 'Click', 'CLIPS', 'Closure-Templates',
+        'CMake', 'COBOL', 'ColdFusion', 'ColdFusion-CFC', 'COLLADA',
+        'Common-Lisp', 'Component-Pascal', 'Cool', 'Coq', 'Cpp-ObjDump',
+        'Creole', 'Crystal', 'CSON', 'Csound', 'Csound-Document', 'Csound-Score',
+        'CSV', 'Cuda', 'CWeb', 'Cycript', 'Cython', 'D', 'D-ObjDump', 'Darcs-Patch',
+        'Dart', 'desktop', 'Diff', 'DIGITAL-Command-Language', 'DM', 'DNS-Zone',
+        'Dockerfile', 'Dogescript', 'DTrace', 'Dylan', 'E', 'Eagle', 'Easybuild',
+        'EBNF', 'eC', 'Ecere-Projects', 'ECL', 'ECLiPSe', 'edn', 'Eiffel', 'EJS',
+        'Elixir', 'Elm', 'Emacs-Lisp', 'EmberScript', 'EQ', 'Erlang', 'F#', 'Factor',
+        'Fancy', 'Fantom', 'Filebench-WML', 'Filterscript', 'fish', 'FLUX', 'Formatted',
+        'Forth', 'Fortran', 'FreeMarker', 'Frege', 'G-code', 'Game-Maker-Language', 'GAS',
+        'GAP', 'GCC-Machine-Description', 'GDB', 'GDScript', 'Genie', 'Genshi', 'Gentoo-Ebuild',
+        'Gentoo-Eclass', 'Gettext-Catalog', 'Gherkin', 'GLSL', 'Glyph', 'GN', 'Gnuplot', 'Golo',
+        'Gosu', 'Grace', 'Gradle', 'Grammatical-Framework', 'Graph-Modeling-Language', 'GraphQL',
+        'Graphviz-(DOT)', 'Groovy', 'Groovy-Server-Pages', 'Hack', 'Haml', 'Handlebars', 'Harbour',
+        'Haxe', 'HCL', 'HLSL', 'HTML+Django', 'HTML+ECR', 'HTML+EEX', 'HTML+ERB', 'HTML+PHP', 'HTTP',
+        'Hy', 'HyPhy', 'IDL', 'Idris', 'IGOR-Pro', 'Inform-7', 'INI', 'Inno-Setup', 'Io', 'Ioke', 'IRC-log',
+        'Isabelle', 'Isabelle-ROOT', 'J', 'Jasmin', 'Java-Server-Pages', 'JFlex', 'Jison',
+        'Jison-Lex', 'Jolie', 'JSON', 'JSON5', 'JSONiq', 'JSONLD', 'JSX', 'Julia', 'Jupyter-Notebook',
+        'KiCad', 'Kit', 'Kotlin', 'KRL', 'LabVIEW', 'Lasso', 'Latte', 'Lean', 'Less', 'Lex',
+        'LFE', 'LilyPond', 'Limbo', 'Linker-Script', 'Linux-Kernel-Module', 'Liquid', 'Literate-Agda',
+        'Literate-CoffeeScript', 'Literate-Haskell', 'LiveScript', 'LLVM', 'Logos', 'Logtalk', 'LOLCODE',
+        'LookML', 'LoomScript', 'LSL', 'M', 'M4', 'M4Sugar', 'Makefile', 'Mako', 'Markdown', 'Marko', 'Mask',
+        'Mathematica', 'Maven-POM', 'Max', 'MAXScript', 'MediaWiki', 'Mercury', 'Meson', 'Metal', 'MiniD', 'Mirah',
+        'Modelica', 'Modula-2', 'Module-Management-System', 'Monkey', 'Moocode', 'MoonScript', 'MQL4', 'MQL5', 'MTML',
+        'MUF', 'mupad', 'Myghty', 'NCL', 'Nemerle', 'nesC', 'NetLinx', 'NetLinx+ERB', 'NetLogo', 'NewLisp', 'Nginx',
+        'Nim', 'Ninja', 'Nit', 'Nix', 'NL', 'NSIS', 'Nu', 'NumPy', 'ObjDump', 'Objective-C++', 'Objective-J',
+        'OCaml', 'Omgrofl', 'ooc', 'Opa', 'Opal', 'OpenCL', 'OpenEdge-ABL', 'OpenRC-runscript', 'OpenSCAD',
+        'OpenType-Feature-File', 'Org', 'Ox', 'Oxygene', 'Oz', 'P4', 'Pan', 'Papyrus', 'Parrot', 'Parrot-Assembly',
+        'Parrot-Internal-Representation', 'Pascal', 'PAWN', 'Pep8', 'Perl-6', 'Pic', 'Pickle',
+        'PicoLisp', 'PigLatin', 'Pike', 'PLpgSQL', 'PLSQL', 'Pod', 'PogoScript', 'Pony', 'PostScript',
+        'POV-Ray-SDL', 'PowerBuilder', 'PowerShell', 'Processing', 'Prolog', 'Propeller-Spin', 'Protocol-Buffer',
+        'Public-Key', 'Pug', 'Puppet', 'Pure-Data', 'PureBasic', 'PureScript', 'Python-console', 'Python-traceback',
+        'QMake', 'QML', 'Racket', 'Ragel', 'RAML', 'Rascal', 'Raw-token-data', 'RDoc', 'REALbasic', 'Reason', 'Rebol',
+        'Red', 'Redcode', 'Regular-Expression', "Ren'Py", 'RenderScript', 'reStructuredText', 'REXX', 'RHTML', 'Ring',
+        'RMarkdown', 'RobotFramework', 'Roff', 'Rouge', 'RPM-Spec', 'RUNOFF', 'Rust', 'Sage', 'SaltStack', 'SAS', 'Sass',
+        'Scaml', 'Scheme', 'Scilab', 'SCSS', 'Self', 'ShaderLab', 'ShellSession', 'Shen', 'Slash', 'Slim', 'Smali',
+        'Smalltalk', 'Smarty', 'SMT', 'SourcePawn', 'SPARQL', 'Spline-Font-Database', 'SQF', 'SQL', 'SQLPL',
+        'Squirrel', 'SRecode-Template', 'Stan', 'Standard-ML', 'Stata', 'STON', 'Stylus', 'Sublime-Text-Config',
+        'SubRip-Text', 'SuperCollider', 'SVG', 'SystemVerilog', 'Tcl', 'Tcsh', 'Tea', 'Terra', 'Text', 'Textile',
+        'Thrift', 'TI-Program', 'TLA', 'TOML', 'Turing', 'Turtle', 'Twig', 'TXL', 'Type-Language', 'TypeScript',
+        'Unified-Parallel-C', 'Unity3D-Asset', 'Unix-Assembly', 'Uno', 'UnrealScript', 'UrWeb', 'Vala', 'VCL',
+        'Verilog', 'VHDL', 'Visual-Basic', 'Volt', 'Vue', 'Wavefront-Material', 'Wavefront-Object', 'Web-Ontology-Language',
+        'WebAssembly', 'WebIDL', 'wisp', 'World-of-Warcraft-Addon-Data', 'X10', 'xBase', 'XC', 'XCompose',
+        'XML', 'Xojo', 'XPages', 'XProc', 'XQuery', 'XS', 'XSLT', 'Xtend', 'Yacc', 'YAML', 'YANG', 'Zephir',
+        'Zimpl']
 
 field_list=[
     'id','name','full_name',
@@ -132,7 +189,7 @@ def NextPage(url,next,last):
         except KeyError as e:
             print e
             sleep(2)
-
+language=lang_popular+lang_others
 # Star Count First top 1000 stars respositories per language
 for lang in language:
     while True:
@@ -145,10 +202,12 @@ for lang in language:
                 WriteCSV(json_parsed,['language','stargazers_count'])
                 try:
                     next = FindLink(response,'next')
-                    NextPage(url,next,[10])
+                    last = FindLink(response,'last')
+                    NextPage(url,next,last)
                     break
                 except KeyError as e:
                     print e
+                    break
             else:
                 raise incompleteError('Not incomplete results, try again')
         except KeyError as e:
