4d5e9599c8fc9a2babc69dcffb01536de6cffc09
Kim Dong Sung <paulkimds@gmail.com>
2017-09-05 22:44:17 +0900

    Update sna_test.py

--- sna_test.py
+++ sna_test.py
@@ -6,34 +6,36 @@ import pandas as pd
 import numpy as np
 import csv
 
-file_path = 'C:\\Users\\LG\\Desktop\\'  # 읽어올 데이터 path 설정
+file_path = 'C:\\Users\\LG\\Desktop\\'  #  로컬저장소 상에 분석용 데이터가 위치한 path 설정
 
 data = pd.read_csv(file_path + 'jongmake.csv')     # pandas 라이브러리를 이용해서 csv 파일 읽어오기
-# print(data.head())
-# print('*'*150)
-# print(data.info())
 
 
-# node 설정
-a_node = data.values[:,0]    # a_node = id
+
+# node 정의
+a_node = data.values[:,0]    # a_node = User ID
 b_node = data.values[:,1]    # b_node = repository_name
 
 
-# edge 설정
+# edge list 정의
 edge_list = []
 for i, j in zip(a_node, b_node):
     temp = (i,j)
     edge_list.append(temp)
-# print(edge_list)
 
-G = nx.Graph()
-user_nodes = list(a_node)
+# print edge list
+print(edge_list)
+
+# 네트워크 분석용 데이터 변환
+user_nodes = list(set(a_node))
 repo_nodes = list(set(b_node))
 nodes = user_nodes + repo_nodes
 edges = [edge_list[0]]
+
+G = nx.Graph()
 G.add_nodes_from(nodes)
 G.add_edges_from(edges)
 
-nx.draw(G)  # 기본 그리기
-
+# edge list Visualization
+nx.draw(G)  
 plt.show()
