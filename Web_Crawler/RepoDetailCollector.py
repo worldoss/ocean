552058987e31ad87a067850a942058e027109776
rlrlaa123 <kimdonghyun3366@gmail.com>
2017-09-08 18:04:33 +0900

    RepoDetailCollector: Fetch Error

--- Web_Crawler/RepoDetailCollector.py
+++ Web_Crawler/RepoDetailCollector.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import urllib
 import csv
 import datetime
+import time
 
 # Other Language 리스트를 웹에서 불러옴 (콘솔에 출력)
 class WebCrawler():
@@ -11,50 +12,63 @@ class WebCrawler():
         self.data = {}
 
     # Request HTML
-    def Request(self,user,repository):
-        fp = urllib.urlopen('https://github.com/'+user+'/'+repository)
+    def Request(self,owner,repository):
+        url = 'https://github.com/'+owner+'/'+repository
+        print url
+        fp = urllib.urlopen(url)
         source = fp.read()
         fp.close()
+        time.sleep(0.2)
         self.request = BeautifulSoup(source, 'html.parser')
 
     # Scrap Summary
     def SummaryScrap(self):
-        summary= self.request.findAll('ul', attrs={'class':'numbers-summary'})
-        sumelement = summary[0].find_all('a')
-        for ele in sumelement:
-            parsed = ele.text.replace("\n","").strip().replace(" ","")
-            if ',' in parsed:
-                parsed = parsed.replace(',','')
-            if 'commits' in parsed:
-                value = parsed.replace('commits','')
-                self.data['Commit'] = int(value)
-                print 'Commits: ' + value
-            elif 'branches' in parsed:
-                value = parsed.replace('branches','')
-                self.data['Branch'] = int(value)
-                print 'Branches: ' + value
-            elif 'releases' in parsed:
-                value = parsed.replace('releases','')
-                self.data['Release'] = int(value)
-                print 'Releases: ' + value
-            elif 'contributors' in parsed:
-                value = parsed.replace('contributors','')
-                self.data['Contributor'] = int(value)
-                print 'Contributors: ' + value
-            else:
-                self.data['License']=parsed
-                print 'License: ' + parsed
-
+        while True:
+            try:
+                summary= self.request.findAll('ul', attrs={'class':'numbers-summary'})
+                sumelement = summary[0].find_all('a')
+                for ele in sumelement:
+                    parsed = ele.text.replace("\n","").strip().replace(" ","")
+                    print parsed
+                    if ',' in parsed:
+                        parsed = parsed.replace(',','')
+                    if 'commits' in parsed:
+                        value = parsed.replace('commits','')
+                        print 'Commits: ' + value
+                        self.data['Commit'] = int(value)
+                    elif 'branches' in parsed:
+                        value = parsed.replace('branches','')
+                        print 'Branches: ' + value
+                        self.data['Branch'] = int(value)
+                    elif 'releases' in parsed:
+                        value = parsed.replace('releases','')
+                        print 'Releases: ' + value
+                        self.data['Release'] = int(value)
+                    elif 'contributors' in parsed:
+                        value = parsed.replace('contributors','')
+                        print 'Contributors: ' + value
+                        self.data['Contributor'] = int(value)
+                    elif 'license' in parsed:
+                        self.data['License']=parsed
+                        print 'License: ' + parsed
+                    else:
+                        print parsed
+                break
+            except:
+                self.Request(owner,repo)
     # Scrap Topics
     def TopicScrap(self):
         self.data['Topic'] = []
         topic = self.request.findAll('div', attrs={'id':'topics-list-container'})
-        topelement = topic[0].find_all('a')
-        print 'Topic: '
-        for ele in topelement:
-            parsed = ele.text.replace('\n','').strip()
-            self.data['Topic'].append(parsed)
-            print parsed
+        if topic:
+            topicelement = topic[0].find_all('a')
+            print 'Topic: '
+            for ele in topicelement:
+                parsed = ele.text.replace('\n', '').strip()
+                self.data['Topic'].append(parsed)
+                print parsed
+        else:
+            print 'no topic'
 
     # Save Results
     def CSVWrtier(self):
@@ -64,19 +78,14 @@ class WebCrawler():
             self.data['Saved_DateTime'] = str(datetime.datetime.now())
             writer.writerow(self.data)
 
-    def RepoParser(self):
-        with open('data/finalRepoDataCol2.csv','r') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                full_name = row['full_name'].split('/')
-                print full_name
-                return full_name
+repositories = WebCrawler()
 
-repositories=WebCrawler()
-user,repo=repositories.RepoParser()
-print user, repo
-repositories.Request('facebook','react')
-repositories.SummaryScrap()
-repositories.TopicScrap()
-print repositories.data
-repositories.CSVWrtier()
+# Parse Repository owner and name
+with open('data/finalRepoDataCol2.csv','r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        owner,repo = row['full_name'].split('/')
+        repositories.Request(owner,repo)
+        repositories.SummaryScrap()
+        repositories.TopicScrap()
+        repositories.CSVWrtier()
