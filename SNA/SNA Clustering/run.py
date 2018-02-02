02208a7cec85d0e8059fcc55368a7a78fdcc4093
rlrlaa123 <kimdonghyun3366@gmail.com>
2018-02-02 22:55:16 +0900

    SNA Clustering: upload code

--- SNA/SNA Clustering/run.py
+++ SNA/SNA Clustering/run.py
@@ -9,14 +9,14 @@ new_repo_topic_data = 'new_repo_topic_data.csv'
 sna = SNACluster(folder_name,classification_translation,new_repo_topic_data)
 
 # SNA 분석을 위한 그래프 생성
-# sna.create_graph()
-# # SNA Clustering 실행
-# sna.clustering()
-# # 각 Cluster의 노드별 Centrality 생성
-# sna.centrality()
-# # 다음 작업을 위한 Centrality 파싱
-# sna.centrality_parser()
-# # 상위 Centrality 별로 Cluster 정렬
-# sna.highest_centrality()
+sna.create_graph()
+# SNA Clustering 실행
+sna.clustering()
+# 각 Cluster의 노드별 Centrality 생성
+sna.centrality()
+# 다음 작업을 위한 Centrality 파싱
+sna.centrality_parser()
+# 상위 Centrality 별로 Cluster 정렬
+sna.highest_centrality()
 # 소프트웨어 재분류 결과
 sna.writing_classification_result()
