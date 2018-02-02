6517f74966809d34c70daaf872471a68f00313a4
rlrlaa123 <kimdonghyun3366@gmail.com>
2018-02-02 13:47:35 +0900

    SNA Clustering: upload coe

---
+++ SNA/SNA Clustering/run.py
@@ -0,0 +1,20 @@
+from sna_clustering import *
+from pprint import pprint
+
+# 데이터가 저장될 폴더명
+folder_name = 'data'
+
+sna = SNACluster(folder_name)
+
+# SNA 분석을 위한 그래프 생성
+sna.create_graph()
+# SNA Clustering 실행
+sna.clustering()
+# 각 Cluster의 노드별 Centrality 생성
+sna.centrality()
+# 다음 작업을 위한 Centrality 파싱
+sna.centrality_parser()
+# 상위 Centrality 별로 Cluster 정렬
+sna.highest_centrality()
+# 소프트웨어 재분류 결과
+sna.writing_classification_result()
